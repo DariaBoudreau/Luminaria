b8fba68a7af9f05e98327a6bd02892f3bdef7c39
Karen Spriggs <k.spriggs01@gmail.com>
2022-02-16 21:05:13 -0600

    nonoiselantern

--- Assets/Scripts/LanternLightingNoNoise.cs
+++ Assets/Scripts/LanternLightingNoNoise.cs
@@ -3,38 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
 
-public class LanternLighting : MonoBehaviour
+public class LanternLightingNoNoise : MonoBehaviour
 {
     new private Light2D light;
     private float maxIntensity;
-    [SerializeField] AudioSource audioSource;
-    [SerializeField] private AudioClip[] lanternMusic;
+    //[SerializeField] AudioSource audioSource;
+    //[SerializeField] private AudioClip[] lanternMusic;
 
+    // Start is called before the first frame update
     void Start()
     {
         light = gameObject.GetComponentInChildren<Light2D>();
         light.enabled = false;
         maxIntensity = light.intensity;
-        audioSource = this.GetComponent<AudioSource>();
+        //udioSource = this.GetComponent<AudioSource>();
     }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Aspen")) {
-            if (light.enabled == false) {
+        if (other.gameObject.CompareTag("Aspen"))
+        {
+            if (light.enabled == false)
+            {
                 light.enabled = true;
                 StartCoroutine(FadeIn());
-                audioSource.PlayOneShot(lanternMusic[UnityEngine.Random.Range(0, lanternMusic.Length)]);
+                //audioSource.PlayOneShot(lanternMusic[UnityEngine.Random.Range(0, lanternMusic.Length)]);
             }
         }
     }
 
     private IEnumerator FadeIn()
     {
-        for (float ft = 0; ft < maxIntensity; ft += 0.1f) {
+        for (float ft = 0; ft < maxIntensity; ft += 0.1f)
+        {
             light.intensity = ft;
             yield return new WaitForSeconds(0.1f);
-            
         }
     }
 }
