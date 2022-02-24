e8fe475760f3d387e9de66bb08e883d0c348eced
Karen Spriggs <k.spriggs01@gmail.com>
2022-02-23 18:41:55 -0600

    adding stone frames

--- Assets/Scripts/StoneHUD.cs
+++ Assets/Scripts/StoneHUD.cs
@@ -9,16 +9,45 @@ public class StoneHUD : MonoBehaviour
     [SerializeField] Image stone2_image;
     [SerializeField] Image stone3_image;
     [SerializeField] Image stone4_image;
-    [SerializeField] PickUps stone1;
-    [SerializeField] PickUps stone2;
-    [SerializeField] PickUps stone3;
-    [SerializeField] PickUps stone4;
 
-    void Update()
+    [SerializeField] Image stone2_circle;
+    [SerializeField] Image stone3_circle;
+    [SerializeField] Image stone4_circle;
+
+    void Start()
     {
-        stone1_image.enabled = stone1.isCollected;
-        stone2_image.enabled = stone2.isCollected;
-        stone3_image.enabled = stone3.isCollected;
-        stone4_image.enabled = stone4.isCollected;
+        stone1_image.enabled = false;
+        stone2_image.enabled = false;
+        stone3_image.enabled = false;
+        stone4_image.enabled = false;
+        stone2_circle.enabled = false;
+        stone3_circle.enabled = false;
+        stone4_circle.enabled = false;
+    }
+
+    public void TurnOnStoneOne()
+    {
+        stone1_image.enabled = true;
+        stone2_circle.enabled = true;
+        stone3_circle.enabled = true;
+        stone4_circle.enabled = true;
+    }
+
+    public void TurnOnStoneTwo()
+    {
+        stone2_image.enabled = true;
+        stone2_circle.enabled = false;
+    }
+
+    public void TurnOnStoneThree()
+    {
+        stone3_image.enabled = true;
+        stone3_circle.enabled = false;
+    }
+
+    public void TurnOnStoneFour()
+    {
+        stone4_image.enabled = true;
+        stone4_circle.enabled = false;
     }
 }
